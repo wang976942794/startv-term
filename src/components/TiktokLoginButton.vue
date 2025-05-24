25271856a6162dec703c77ac2e09821ba06f2ee7
wwang976942794 <wmyqwer1234@163.com>
2025-05-24 11:51:23 +0800

    updata tiktok

--- src/components/TiktokLoginButton.vue
+++ src/components/TiktokLoginButton.vue
@@ -16,7 +16,7 @@ const { t } = useI18n()
 const userStore = useUserStore()
 const emit = defineEmits(['login-success'])
 const clientKey = 'sbawwjfsvg5z0lacsi'
-const authCode = ref('');
+
 function generateRandomString(length) {
   let result = ''
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
@@ -32,48 +32,13 @@ const handleTiktokLogin = () => {
   const authUrl = `https://www.tiktok.com/v2/auth/authorize?client_key=${clientKey}&response_type=code&scope=user.info.basic&redirect_uri=${encodeURIComponent(window.location.origin)}&state=${state}`
   window.location.href = authUrl
 }
-// 用授权码交换访问令牌
-const exchangeCodeForToken = async (code) => {
-  try {
-    // 注意：实际项目中应该调用你的后端API，而不是直接在前端使用client_secret
-    const response = await axios.post('https://open.tiktokapis.com/v2/oauth/token/', {
-      client_key: clientKey,
-      client_secret: clientSecret,
-      code: code,
-      grant_type: 'authorization_code',
-      redirect_uri: redirectUri
-    });
-    
-    const accessToken = response.data.access_token;
-    await fetchUserInfo(accessToken);
-  } catch (error) {
-    console.error('获取访问令牌失败:', error);
-  }
-};
-// 检查URL中是否有授权码（回调后处理）
-const checkAuthCode = () => {
-  const urlParams = new URLSearchParams(window.location.search);
-  const code = urlParams.get('code');
-  const state = urlParams.get('state');
-  
-  if (code && state) {
-    const savedState = localStorage.getItem('tiktok_auth_state');
-    if (state === savedState) {
-      authCode.value = code;
-      exchangeCodeForToken(code);
-      // 清除URL中的参数
-      window.history.replaceState({}, document.title, window.location.pathname);
-    }
-  }
-};
 
 onMounted(async () => {
-  checkAuthCode()
-  // const urlParams = new URLSearchParams(window.location.search)
-  // const code = urlParams.get('code')
-  // const state = urlParams.get('state')
-  // console.log("code",code);
-  // console.log("state",state);
+  const urlParams = new URLSearchParams(window.location.search)
+  const code = urlParams.get('code')
+  const state = urlParams.get('state')
+  console.log("code",code);
+  console.log("state",state);
   // if (code && state) {
   //   const savedState = localStorage.getItem('tiktok_auth_state')
   //   if (state === savedState) {
